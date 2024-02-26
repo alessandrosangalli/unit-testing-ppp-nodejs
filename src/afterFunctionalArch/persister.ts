a49e0ffa01758d29c29606696badc6bd0530ac8f
Alessandro Sangalli <alessandrosangalli@vipcommerce.com.br>
2024-02-25 22:59:50 -0300

    teste

--- src/afterFunctionalArch/persister.ts
+++ src/afterFunctionalArch/persister.ts
@@ -2,7 +2,7 @@ import { FileContent, FileUpdate } from './audit-manager';
 import * as fsPromises from 'fs/promises';
 import * as path from 'path';
 
-// Camada mais externa com efeitos colaterais
+// Camada mais externa com efeitos colaterais -> podemos testar utilizando mocks caso necessário
 export class Persister {
   public async readDirectory(directoryName: string): Promise<FileContent[]> {
     const files = await fsPromises.readdir(directoryName);
